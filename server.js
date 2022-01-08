5efb74ab38f9e8b861e5ab7d3edd901d276ed201
kumarkrsjs <kumarkrsjs@protonmail.com>
2022-01-08 12:33:54 -0800

    add login POST route

--- server.js
+++ server.js
@@ -17,6 +17,29 @@ app.get('/users/:id', function(req, res){
     )
 })
 
+app.post('/login', function(re, res){
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    // This should come from the database
+    const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+
+    if(username === mockUsername && password == mockPassword){
+        re.json({
+            success:true,
+            message: 'password and username match!',
+			token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+    }
+
+})
+
 app.listen(8000, function(){
     console.log("server is running")
 })
